0833041f88a0fc5bb41c5d590637590d9fa4abc7
muhumbania <franklinmuhumbania@gmail.com>
2023-10-23 21:52:29 +0300

    Added the start a new post button

--- show.py
+++ show.py
@@ -1,6 +1,13 @@
-import numpy as np
+# import numpy as np
+# import matplotlib.pyplot as plt
 
-def make(n):
-    x = np.random.randn(n)
-    y = np.random.randn(n)
-    return x , y
+# def make(n):
+#     x = np.random.randn(n)
+#     y = np.random.randn(n)
+    
+#     fig, ax = plt.subplots()
+#     ax.scatter(x, y)
+#     fig
+
+def m():
+    print('Hello world')
