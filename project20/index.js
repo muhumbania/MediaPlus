f2459a549a4becb96e238e145cd7f2cfde9620e6
muhumbania <franklinmuhumbania@gmail.com>
2023-12-14 18:39:10 +0300

    added the footer div

---
+++ project20/index.js
@@ -0,0 +1,15 @@
+
+// const buffer = ['', 'home'];
+
+// const hIcons = document.querySelectorAll('.h-icon');
+
+console.log('bonjour');
+
+// hIcons.forEach(function(hIcon){
+//     hIcon.addEventListener('click', function(){
+//         console.log('Got clicked');
+//     });
+// });
+
+
+
