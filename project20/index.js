15f248aa90e38cb014648eff8957a857f7ab0229
muhumbania <franklinmuhumbania@gmail.com>
2023-12-17 23:31:12 +0300

    Addded the anniamation for inversing header icons

--- project20/index.js
+++ project20/index.js
@@ -7,17 +7,15 @@ const newPost = document.querySelectorAll('.newPost');
 const startPostButton = document.getElementById('startPost');
 document.querySelector('.postForm').addEventListener('click', event => event.stopPropagation());
 
+//Account icon element
 const accountIcon = document.querySelector('.account');
 document.querySelector('.accountDiv').addEventListener('click', event => event.stopPropagation());
 
-// newPost.forEach(function(newPost){
-//     newPost.classList.add('hidden');
-// });
+//Header icons
+const headerIcons = document.querySelectorAll('.h-icon');
 
-let startPost;
-
+//Function for new post animation
 function display(state){
-    let displayPost;
     if(state === true){
         newPost.forEach(function(newPost){
             newPost.classList.remove('hidden');
@@ -31,21 +29,41 @@ function display(state){
     }
 }
 
+//Display the postForm when the Start post button gets clicked
 startPostButton.addEventListener('click', function(event){
     event.stopPropagation();
-    startPost = true;
-    display(startPost);
+    display(true);
 });
 
+//Display the account div when the account icon gets clicked
 accountIcon.addEventListener('click', function(event){
     event.stopPropagation();
     document.querySelector('.accountDiv').classList.remove('hidden');
 });
 
+//Hide every pop up divs when the window gets clicked
 window.addEventListener('click', function(){
-    startPost = false;
-    display(startPost);
+    display(false);
     document.querySelector('.accountDiv').classList.add('hidden');
+});
+
+
+const buffer = ['1', '1'];
+headerIcons.forEach(function(headerIcon){
+    
+    headerIcon.addEventListener('click', function(){
+        buffer.push(`${this.id}`);
+        buffer.shift();
+        
+        if(buffer[0] !== buffer[1]){
+            document.querySelector(`.i${buffer[1]}`).setAttribute('type', 'solid');
+            document.querySelector(`.i${buffer[0]}`).setAttribute('type', 'regular');
+        }else{
+            document.querySelector(`.i${buffer[0]}`).setAttribute('type', 'solid');
+        }
+
+        // this.classList.add('border-b-2 border-blue-500');
+    });
 });
 
 
