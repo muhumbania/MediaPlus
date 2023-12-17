393fe44f0e771f78f704d43c68d4e34576ac27d6
muhumbania <franklinmuhumbania@gmail.com>
2023-12-17 21:38:22 +0300

    Added the animation for the new post button

--- project20/index.js
+++ project20/index.js
@@ -1,15 +1,49 @@
 
-// const buffer = ['', 'home'];
+const body = document.getElementById('body');
 
-// const hIcons = document.querySelectorAll('.h-icon');
+// body.classList.remove('overflow-hidden');
 
-console.log('bonjour');
+const newPost = document.querySelectorAll('.newPost');
 
-// hIcons.forEach(function(hIcon){
-//     hIcon.addEventListener('click', function(){
-//         console.log('Got clicked');
-//     });
+const startPostButton = document.getElementById('startPost');
+
+document.querySelector('.postForm').addEventListener('click', event => event.stopPropagation() );
+
+// newPost.forEach(function(newPost){
+//     newPost.classList.add('hidden');
 // });
+
+let startPost;
+
+function display(state){
+    let displayPost;
+    if(state === true){
+        newPost.forEach(function(newPost){
+            newPost.classList.remove('hidden');
+        });
+        body.classList.add('overflow-hidden');
+    }else{
+        newPost.forEach(function(newPost){
+            newPost.classList.add('hidden');
+        });
+        body.classList.remove('overflow-hidden');
+    }
+}
+
+startPostButton.addEventListener('click', function(event){
+    event.stopPropagation();
+    startPost = true;
+    display(startPost);
+});
+
+
+
+window.addEventListener('click', function(){
+    startPost = false;
+    display(startPost);
+});
+
+
 
 
 
