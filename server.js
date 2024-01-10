690599f46ccdb4eb9efc9cdda3e91717484e3e05
muhumbania <franklinmuhumbania@gmail.com>
2024-01-10 23:17:41 +0300

    Used the routes in the server file

--- server.js
+++ server.js
@@ -1,33 +1,20 @@
 import  express  from "express";
+import indexRoutes from './routes/indexRoutes.js';
+import dashboardRoutes from './routes/dashboardRoutes.js';
+import notificationsRoutes from './routes/notificationsRoutes.js';
+import scheduleRoutes from './routes/scheduleRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res)=>{
-    res.render('index');
-});
-
-app.get('/dashboard', (req, res)=>{
-    res.render('dashboard');
-});
-
-app.get('/notifications', (req, res)=>{
-    res.render('notifications');
-});
-
-app.get('/schedule', (req, res)=>{
-    res.render('schedule');
-});
-
-app.get('/login', (req, res)=>{
-    res.render('signin');
-});
-
-app.get('/register', (req, res)=>{
-    res.render('signup');
-});
+app.use('/', indexRoutes);
+app.use('/', dashboardRoutes);
+app.use('/', notificationsRoutes);
+app.use('/', scheduleRoutes);
+app.use('/', authRoutes);
 
 app.listen(3000 , ()=>{
     console.log(`server listening to port ${port}`);
