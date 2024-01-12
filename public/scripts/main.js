d8fae8f38b9c96ac1a851a5f649e161b93799436
muhumbania <franklinmuhumbania@gmail.com>
2024-01-12 18:39:41 +0300

    Completed the post button animation

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,20 @@
+const postButton = document.getElementById('postButton');
+const postContent = document.getElementById('postContent');
+
+postButton.style.pointerEvents = "none";
+postContent.addEventListener('input', function(){
+    if (postContent.value.trim() !== "") {
+        // postButton.disabled = false;
+        postButton.classList.add('bg-blue-500');
+        postButton.classList.add('text-white');
+        postButton.classList.add('cursor-pointer');
+        postButton.style.pointerEvents = "auto";
+      } else {
+        postButton.classList.remove('bg-blue-500');
+        postButton.classList.remove('text-white');
+        postButton.style.pointerEvents = "none";
+      }
+});
 
 
 
