04a44ffa99f28b3bb75e25ed2a8ebe0d1f7587c3
muhumbania <franklinmuhumbania@gmail.com>
2024-01-12 22:40:08 +0300

    Added the facebook notification route

--- routes/notificationsRoutes.js
+++ routes/notificationsRoutes.js
@@ -5,4 +5,8 @@ router.get('/notifications', (req, res)=>{
     res.render('notifications', {currentPage: 'notifications'});
 });
 
+router.get('/notifications/facebook', (req, res)=>{
+    res.render('notifications', {currentPage: 'notifications'});
+});
+
 export default router;
