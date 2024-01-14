fe765dcb866c4d7ee2e8da850c2e54ed4dffe3e8
muhumbania <franklinmuhumbania@gmail.com>
2024-01-14 19:15:24 +0300

    Added the routes for the dashboard dropdown

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,8 +1,33 @@
 import  express  from "express";
 const router = express.Router();
 
-router.get('/dashboard', (req, res)=>{
-    res.render('dashboard', {currentPage: 'dashboard'});
+router.get('/dashboard/facebook', (req, res)=>{
+    res.render('dashboard', {
+        currentPage: 'dashboard',
+        option: 'facebook'
+    });
+});
+
+
+router.get('/dashboard/linkedin', (req, res)=>{
+    res.render('dashboard', {
+        currentPage: 'dashboard',
+        option: 'linkedin'
+    });
+});
+
+router.get('/dashboard/instagram', (req, res)=>{
+    res.render('dashboard', {
+        currentPage: 'dashboard',
+        option: 'instagram'
+    });
+});
+
+router.get('/dashboard/x', (req, res)=>{
+    res.render('dashboard', {
+        currentPage: 'dashboard',
+        option: 'x'
+    });
 });
 
 export default router;
