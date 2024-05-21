6d518d08d433e17a74baa5c48529ffcdede872a1
muhumbania <franklinmuhumbania@gmail.com>
2024-05-21 22:18:25 +0300

    Updated the icons of the facebook overview

--- public/scripts/facebookAuth.js
+++ public/scripts/facebookAuth.js
@@ -2,17 +2,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
 import { getAuth, signInWithPopup, FacebookAuthProvider } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js";
 console.log("Importing firebaseConfig from:", './firebaseConfig.js');
-// import firebaseConfig from "../firebaseCredentials/firebaseConfig";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyBkkdjq_u9XG3veDRgvr-KdQFMpvhr5FbY",
-    authDomain: "umaizi.firebaseapp.com",
-    projectId: "umaizi",
-    storageBucket: "umaizi.appspot.com",
-    messagingSenderId: "746191270515",
-    appId: "1:746191270515:web:688b88d68ec4601a72b006",
-    measurementId: "G-HWV86R455H"
-};
+import firebaseConfig from "../firebaseCredentials/firebaseConfig";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
