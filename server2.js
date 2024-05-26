b334aefa59544a1b0350a90ac25516455f98eaee
muhumbania <franklinmuhumbania@gmail.com>
2024-05-27 00:15:41 +0300

    --

--- server2.js
+++ server2.js
@@ -7,6 +7,9 @@ import scheduleRoutes from './routes/scheduleRoutes.js';
 
 import express from "express";
 import bodyParser from "body-parser";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const server = express();
 const PORT = process.env.PORT || 3000;
@@ -21,6 +24,14 @@ server.use('/', indexRoutes);
 server.use('/', dashboardRoutes);
 server.use('/', notificationsRoutes);
 server.use('/', scheduleRoutes);
+
+server.get('/config', (req, res) => {
+    res.json({
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabaseKey: process.env.SUPABASE_KEY,
+      linkedInClientId: process.env.LINKEDIN_CLIENT_ID,
+    });
+});
 
 server.listen(PORT , ()=>{
     console.log(`server listening to port ${PORT}`);
